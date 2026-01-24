c0df3d3cd2dd86882770c521d25854716d658382
Elliott CHEN <chen.elliott@gmail.com>
2026-01-24 16:51:02 +0800

    Rich config for catppuccin theme plugin

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -4,12 +4,61 @@ return {
   priority = 1000,
   config = function()
     require('catppuccin').setup {
-      flavour = 'mocha',
+      flavour = 'auto',
       background = {
         light = 'latte',
         dark = 'mocha',
       },
       transparent_background = true,
+      float = {
+        transparent = true,
+        solid = false,
+      },
+      show_end_of_buffer = false,
+      term_colors = false,
+      dim_inactive = { enabled = true, shade = 'dark', percentage = 0.15 },
+      no_italic = false,
+      no_bold = false,
+      no_underline = false,
+      styles = {
+        comments = { 'italic' },
+        conditionals = { 'italic' },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+        -- miscs = {}, -- Uncomment to turn off hard-coded styles
+      },
+      lsp_styles = { -- Handles the style of specific lsp hl groups (see `:h lsp-highlight`).
+        virtual_text = {
+          errors = { 'italic' },
+          hints = { 'italic' },
+          warnings = { 'italic' },
+          information = { 'italic' },
+          ok = { 'italic' },
+        },
+        underlines = {
+          errors = { 'underline' },
+          hints = { 'underline' },
+          warnings = { 'underline' },
+          information = { 'underline' },
+          ok = { 'underline' },
+        },
+        inlay_hints = {
+          background = true,
+        },
+      },
+      color_overrides = {},
+      custom_highlights = {},
+      default_integrations = true,
+      auto_integrations = true,
+      --[[
       integrations = {
         cmp = true,
         gitsigns = true,
@@ -21,6 +70,7 @@ return {
           indentscope_color = '',
         },
       },
+      --]]
     }
 
     vim.cmd.colorscheme 'catppuccin'
